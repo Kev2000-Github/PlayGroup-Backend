17395f9a3ccc92de48452369119c472d26c88fc7
Kevin <chengkev2000@gmail.com>
2021-04-12 12:14:22 -0400

    removed unnecesary alerts

--- dist/config/corsConfig.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.corsConfig = void 0;
-const frontURL = process.env.FRONT_URL || "localhost";
-exports.corsConfig = {
-    origin: [new RegExp(frontURL)],
-    credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE']
-};
--- dist/routes/tictactoe/tictactoe.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.tictactoeRouter = void 0;
-const express_1 = require("express");
-const router = express_1.Router();
-exports.tictactoeRouter = router;
-router.get("/", (req, res) => {
-    res.send("HELLO WORLD");
-});
